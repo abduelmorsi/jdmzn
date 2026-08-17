bde6e691e9562349370ceb156f2abee28e130deb
abduelmorsi <abdelrahman.elmorsi@std.yildiz.edu.tr>
2026-08-17 19:36:20 +0300

    Switch to sveltia cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel';
 import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    keystatic(),
-    markdoc()
-  ],
+  integrations: [markdoc()],
   output: 'static',
   adapter: vercel()
 });
